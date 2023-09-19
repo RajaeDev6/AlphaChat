1e646157a12cc89a6f8a2c217027e7ee75fa3123
rajae <rgivans18@gmail.com>
2023-09-18 21:39:55 -0500

    update ui

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,28 +1,78 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const messageInput = document.getElementById("messageInput") as HTMLInputElement;
-  const sendButton = document.getElementById("sendButton");
+const ws = new WebSocket("ws://localhost:8080")
 
-  if (sendButton && messageInput) {
-    sendButton.addEventListener("click", () => {
-      const messageText = messageInput.value.trim();
-      if (messageText !== "") {
-        // Create a new message element
-        const message = document.createElement("div");
-        message.className = "message sender"; // You can adjust the class here
-        message.innerHTML = `<p>${messageText}</p>`;
+const messageInput = document.getElementById("messageInput") as HTMLInputElement;
+const submitButton = document.getElementById("submitButton") as HTMLButtonElement;
+const messageContainer = document.querySelector(".message-box") as HTMLDivElement;
 
-        // Append the new message to the conversation
-        const convo = document.querySelector(".convo");
-        if (convo) {
-          convo.appendChild(message);
+const contact = document.querySelectorAll(".contact")
 
-          // Clear the input field
-          messageInput.value = "";
+contact.forEach((c) => {
+	c.addEventListener("click", (_) => {
+		updateUI({user: "sender", text: "clicked"})
+	})
+})
 
-          // Automatically scroll to the bottom to show the latest message
-          convo.scrollTop = convo.scrollHeight;
+
+type Message = {
+	user: "sender" | "receiver"
+	text: string
+}
+
+
+
+ws.addEventListener("message", (msg: MessageEvent) => {
+    if (Array.isArray(msg.data)) {
+		if (msg.data.length <= 0) return
+        for (const buffer of msg.data) {
+            if (buffer.isBuffer(buffer)) {
+                const text = buffer.toString('utf-8');
+                updateUI({ user: "receiver", text });
+            }
         }
-      }
-    });
-  }
+    } else if (msg.data.length > 0) {
+			msg.data.forEach((message: Message) => {	
+        		updateUI({ user: "receiver", text: message.text});
+			})
+    }
+
 });
+
+
+
+
+// update the ui by adding message to the screen
+const updateUI = (msg: Message) => {
+    const message = document.createElement("div");
+
+    message.classList.add("message", msg.user);
+    message.textContent = msg.text;
+
+    messageContainer.appendChild(message);
+	messageContainer.scrollTop = messageContainer.scrollHeight
+};
+
+
+// Clears the message input field
+const clearInputField = () => {
+	messageInput.value = ""
+}
+
+// sends the message after user clicks send message
+const submitHandler = (event: Event) => {
+
+    event.preventDefault();
+    const inputElement = messageInput?.value;
+	// check if the message input element exist 
+    if (inputElement) {
+            updateUI({user: "sender", text: inputElement});
+			ws.send(JSON.stringify(inputElement));
+			// ws.send(inputElement)
+			clearInputField();
+        }
+    
+};
+
+// Event for the send message button
+submitButton.addEventListener("click", submitHandler);
+
+export {};
